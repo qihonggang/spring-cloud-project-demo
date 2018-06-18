4cc53fe2c1ba5ce1e455459b6a12d021d9043d14
qhg <sxxzqhg@163.com>
2018-06-19 02:10:25 +0800

    服务消费者（Ribbon+restTemplate），实现客户端负载均衡

---
+++ service-ribbon/src/main/java/com/example/serviceribbon/service/HelloService.java
@@ -0,0 +1,15 @@
+package com.example.serviceribbon.service;
+
+/**
+ * @author 祁宏钢
+ * @date 2018/6/19
+ */
+public interface HelloService {
+    /**
+     * 提供service-hi的负债均衡服务
+     * @param name
+     * @return 服务内容
+     */
+    String hiService(String name);
+
+}
---
+++ service-ribbon/src/main/java/com/example/serviceribbon/service/impl/HelloServiceImpl.java
@@ -0,0 +1,22 @@
+package com.example.serviceribbon.service.impl;
+
+import com.example.serviceribbon.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author 祁宏钢
+ * @date 2018/6/19
+ */
+@Service
+public class HelloServiceImpl implements HelloService {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @Override
+    public String hiService(String name) {
+        return restTemplate.getForObject("http://SERVICE-HI/hi?name="+name,String.class);
+    }
+}
