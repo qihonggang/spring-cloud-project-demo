580950dd14a7f80c30651e5aff12ba2d0391198a
qhg <sxxzqhg@163.com>
2018-06-20 02:50:46 +0800

    服务消费者（Feign）

---
+++ service-feign/src/main/java/com/example/servicefeign/service/SchedualServiceHi.java
@@ -0,0 +1,16 @@
+package com.example.servicefeign.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author 祁宏钢
+ * @date 2018/6/20
+ */
+@FeignClient(value = "service-hi")
+public interface SchedualServiceHi {
+    @RequestMapping(value = "/hi", method = RequestMethod.GET)
+    String sayHiFromClientOne(@RequestParam(value = "name") String name);
+}
